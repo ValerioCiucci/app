937540a9394f9fbd278e469838da1d6aff07f11f
Valerio <valeriociucci4269@gmail.com>
2024-09-24 13:35:37 +0200

    beta

--- sw.js
+++ sw.js
@@ -1,61 +1,39 @@
-const cacheName = 'v1';
-
-
-const cacheAssets = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
-  '/img/gatto.gif'
-];
-
-
 self.addEventListener('install', (event) => {
-  console.log('Service Worker: Installazione');
-
   event.waitUntil(
-    caches
-      .open(cacheName)
-      .then((cache) => {
-        console.log('Service Worker: Caching File');
-        cache.addAll(cacheAssets);
-      })
-      .then(() => self.skipWaiting())
-  );
-});
-
-
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker: Attivazione');
-  
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== cacheName) {
-            console.log('Service Worker: Pulizia vecchia cache');
-            return caches.delete(cache);
-          }
-        })
-      );
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/script.js',
+        '/img/cute.png',
+        '/img/cute1.png'
+      ]);
     })
   );
 });
 
-
 self.addEventListener('fetch', (event) => {
+  const requestUrl = new URL(event.request.url);
+
+  // Filtro per evitare risorse 'chrome-extension://'
+  if (requestUrl.protocol === 'chrome-extension:') {
+    return;
+  }
+
   event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-       
-        const resClone = response.clone();
-        
-        caches.open(cacheName).then((cache) => {
-         
-          cache.put(event.request, resClone);
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).then((networkResponse) => {
+        // Filtro per evitare di cache-izzare risposte parziali (status code 206)
+        if (networkResponse.status === 206) {
+          return networkResponse;  // Non mettere in cache le risposte parziali
+        }
+
+        return caches.open('v1').then((cache) => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
         });
-        return response;
-      })
-      .catch(() => caches.match(event.request).then((response) => response))
+      });
+    })
   );
 });
