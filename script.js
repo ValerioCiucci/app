826c09d21017176cc8dde72cd2f88dd134d62348
Valerio <valeriociucci4269@gmail.com>
2024-09-26 00:27:17 +0200

    beta 3.4

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
     navigator.serviceWorker
@@ -11,11 +10,10 @@ if ('serviceWorker' in navigator) {
       });
   });
 }
-
+var thickness=5;
 
 $(".n").hide();
 $('canvas').hide();
-
 
 var mouse = true;
 let lastX = 0;
@@ -24,21 +22,18 @@ var timeout = [];
 var audio = $("audio")[0];
 audio.volume = 0.2;
 
-
 function clearTimeouts() {
   if (timeout.length > 0) {
-    for (i = 0; i < timeout.length; i++) {
+    for (let i = 0; i < timeout.length; i++) {
       clearTimeout(timeout[i]);
     }
     timeout = [];
   }
 }
 
-
 function accarezza() {
-  if (mouse == true) {
+  if (mouse) {
     clearTimeouts();
-
     $(".d").children().t_off(true);
     $(".gatto img").attr('src', `img/accarezza.gif`);
     $(".d").children().t("SMETTILA DI ACCAREZZARMI");
@@ -51,16 +46,13 @@ function accarezza() {
   }
 }
 
-
 $(".d").hide().fadeIn(200);
 $(".d").children().t("HO VOGLIA DI UN BUBBLE TEA");
 $("audio").hide();
 
-
 function createParticle(x, y) {
   const particle = $('<div class="particle"></div>');
   $('.gatto').append(particle);
-
   particle.css({
     left: x + 'px',
     top: y + 'px',
@@ -75,7 +67,6 @@ function createParticle(x, y) {
   }, 100);
 }
 
-
 $(".gatto img").on('mousemove', function (e) {
   createParticle(e.pageX, e.pageY);
 });
@@ -88,143 +79,129 @@ $(".gatto img").on('touchmove', function (event) {
   });
 });
 
-
 $(".gatto img").on('touchstart', function (e) {
-  if (mouse == true) {
+  if (mouse) {
     const touch = e.touches[0];
-    $('.particle').css({
-      left: touch.pageX + 'px',
-      top: touch.pageY + 'px',
-      opacity: 1
-    });
-
+    createParticle(touch.pageX, touch.pageY);
     clearTimeouts();
     $(".d").children().t_off(true);
     $(".gatto img").attr('src', `img/accarezza.gif`);
     $(".d").children().t("SMETTILA DI ACCAREZZARMI");
 
-    i = setInterval(() => {
+    let i = setInterval(() => {
       createParticle(touch.pageX, touch.pageY);
     }, 100);
+    $(this).data("intervalId", i); 
   }
 });
 
-
-$(".gatto img").on('touchend', function (e) {
-  if (mouse == true) {
-    $('.particle').css('opacity', '0'); 
-
+$(".gatto img").on('touchend', function () {
+  if (mouse) {
+    $('.particle').css('opacity', '0');
     timeout.push(setTimeout(function () {
       $(".gatto img").attr('src', `img/gatto.gif`);
       $(".d").children().t_off(true);
       $(".d").children().t("HO VOGLIA DI UN BUBBLE TEA");
     }, 5000));
 
-    clearInterval(i);
+    clearInterval($(this).data("intervalId")); 
   }
 });
 
-
-if (mouse == true) {
+if (mouse) {
   $(".gatto img").on('mouseenter', accarezza);
 }
 
-
 $(`.box[pid='3']`).click(function () {
-  if (mouse==true){
-  clearTimeouts();
-  $(".d").children().t_off(true);
-  $(".gatto img").attr('src', `img/triste.gif`);
-  $(".d").children().t("NON ESSERE TRISTE, O LO SARO' ANCH'IO");
-
-  timeout.push(setTimeout(function () {
-    $(".gatto img").attr('src', `img/gatto.gif`);
+  if (mouse) {
+    clearTimeouts();
     $(".d").children().t_off(true);
-    $(".d").children().t("HO VOGLIA DI UN BUBBLE TEA");
-  }, 5000));
+    $(".gatto img").attr('src', `img/triste.gif`);
+    $(".d").children().t("NON ESSERE TRISTE, O LO SARO' ANCH'IO");
 
-}
+    timeout.push(setTimeout(function () {
+      $(".gatto img").attr('src', `img/gatto.gif`);
+      $(".d").children().t_off(true);
+      $(".d").children().t("HO VOGLIA DI UN BUBBLE TEA");
+    }, 5000));
+  }
 });
 
-
 $(`.box[pid='4']`).click(function () {
-  mouse = false; 
-  clearTimeouts();
 
+  mouse = false;
+  clearTimeouts();
   $(".d").children().t_off(true);
   $(".gatto img").attr('src', `img/triste2.gif`);
   $(".d").children().t("HO CAPITO ME NE VADO");
 
- 
   $(".gatto img").fadeOut(5000);
   $(".d").fadeOut(5000);
   $(".box").hide(5000);
 
   timeout.push(setTimeout(function () {
     $(".n").toggle(1000);
+    enableDrawing()
   }, 5000));
 });
 
-
 $(`.box[pid='1']`).click(function () {
-  if (mouse==true){
-  clearTimeouts();
-  $(".d").children().t_off(true);
-  $(".gatto img").attr('src', `img/love.gif`);
-  $(".d").children().t("GRAZIEEEE");
-
-  timeout.push(setTimeout(function () {
-    $(".gatto img").attr('src', `img/beve.gif`);
+  if (mouse) {
+    clearTimeouts();
     $(".d").children().t_off(true);
-    $(".d").children().t("GLU GLU GLU");
+    $(".gatto img").attr('src', `img/love.gif`);
+    $(".d").children().t("GRAZIEEEE");
 
-    setTimeout(function () {
-      $(".gatto img").attr('src', `img/gatto.gif`);
+    timeout.push(setTimeout(function () {
+      $(".gatto img").attr('src', `img/beve.gif`);
       $(".d").children().t_off(true);
-      $(".d").children().t("HO VOGLIA DI UN BUBBLE TEA");
-    }, 5000);
-  }, 5000));
+      $(".d").children().t("GLU GLU GLU");
+
+      setTimeout(function () {
+        $(".gatto img").attr('src', `img/gatto.gif`);
+        $(".d").children().t_off(true);
+        $(".d").children().t("HO VOGLIA DI UN BUBBLE TEA");
+      }, 5000);
+    }, 5000));
   }
 });
 
-
 $(`.box[pid='2']`).click(function () {
-  if (mouse==true){
-  clearTimeouts();
-  $(".d").children().t_off(true);
-  $(".gatto img").attr('src', `img/arrabbiato.gif`);
-  $(".d").children().t("SEI UNO STRONZOO!");
-
-  timeout.push(setTimeout(function () {
-    $(".gatto img").attr('src', `img/gatto.gif`);
+  if (mouse) {
+    clearTimeouts();
     $(".d").children().t_off(true);
-    $(".d").children().t("HO VOGLIA DI UN BUBBLE TEA");
-  }, 5000));
-}
-});
+    $(".gatto img").attr('src', `img/arrabbiato.gif`);
+    $(".d").children().t("SEI UNO STRONZOO!");
 
+    timeout.push(setTimeout(function () {
+      $(".gatto img").attr('src', `img/gatto.gif`);
+      $(".d").children().t_off(true);
+      $(".d").children().t("HO VOGLIA DI UN BUBBLE TEA");
+    }, 5000));
+  }
+});
 
 let drawingEnabled = false;
 const $canvas = $('canvas')[0];
 const ctx = $canvas.getContext('2d');
 let color = $('input')[0].value;
-let firstTouch=false;
+let firstTouch = false;
+let back =[]
+let forward=[];
+
 
 function draw(x, y) {
-  
   ctx.fillStyle = color;
   ctx.strokeStyle = color;
-  ctx.lineWidth = 5;
+  ctx.lineWidth = thickness;
   ctx.lineCap = 'round';
-if(firstTouch==false){
-  ctx.lineTo(x, y);  
-   
-}
-else{
-  ctx.moveTo(x, y); 
-  firstTouch=false;
-}  
- 
+
+  if (firstTouch) {
+    ctx.moveTo(x, y);
+    firstTouch = false; 
+  } else {
+    ctx.lineTo(x, y);
+  }
 
   ctx.stroke();
   lastX = x;
@@ -232,7 +209,7 @@ else{
 }
 
 function enableDrawing() {
-  ctx.beginPath()
+  ctx.beginPath();
   color = $('input')[0].value;
   $('canvas').show();
   drawingEnabled = true;
@@ -240,10 +217,19 @@ function enableDrawing() {
 
 $('.box[pid=6]').on('click', enableDrawing);
 
+$canvas.addEventListener('mousedown', function (e) {
+  back .push(ctx.getImageData(0, 0, $canvas.width, $canvas.height));
 
-$canvas.addEventListener('mousedown', function(e) {
+  firstTouch = true; 
   let draw1 = true;
-  $canvas.addEventListener('mousemove', function(e) {
+
+  const rect = $canvas.getBoundingClientRect();
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+
+  draw(x, y); 
+
+  $canvas.addEventListener('mousemove', function (e) {
     if (draw1) {
       const rect = $canvas.getBoundingClientRect();
       const x = e.clientX - rect.left;
@@ -251,47 +237,71 @@ $canvas.addEventListener('mousedown', function(e) {
       draw(x, y);
     }
   });
-  $canvas.addEventListener('mouseup', function() {
+
+  $canvas.addEventListener('mouseup', function () {
     draw1 = false;
-    firstTouch=true;
-    
-  });
+  }, { once: true }); 
 });
 
-$canvas.addEventListener('touchstart', function(e) {
+$canvas.addEventListener('touchstart', function (e) {
+  back .push( ctx.getImageData(0, 0, $canvas.width, $canvas.height));
+  firstTouch = true; 
   let draw1 = true;
-$canvas.addEventListener('touchmove', function(e) {
-  if (draw1) {
-    e.preventDefault();
-    const rect = $canvas.getBoundingClientRect();
-    const touch = e.touches[0];
-    const x = touch.clientX - rect.left;
-    const y = touch.clientY - rect.top;
-    draw(x, y);
-  }
+
+  const rect = $canvas.getBoundingClientRect();
+  const touch = e.touches[0];
+  const x = touch.clientX - rect.left;
+  const y = touch.clientY - rect.top;
+
+  draw(x, y); 
+
+  $canvas.addEventListener('touchmove', function (e) {
+   
+    if (draw1) {
+      e.preventDefault();
+      const rect = $canvas.getBoundingClientRect();
+      const touch = e.touches[0];
+      const x = touch.clientX - rect.left;
+      const y = touch.clientY - rect.top;
+      draw(x, y);
+    }
+  });
+
+  $canvas.addEventListener('touchend', function () {
+    draw1 = false;
+    
+  }, { once: true });
 });
 
-$canvas.addEventListener('touchend', function() {
-  draw1 = false;
-  firstTouch=true;
-  
-});
-
-});
-
-$(`.box[pid='8']`).click(function() {
+$(`.box[pid='8']`).click(function () {
   ctx.clearRect(0, 0, $canvas.width, $canvas.height);
-  ctx.beginPath()
+  ctx.beginPath();
 });
 
+$(`.box[pid='11']`).click(function () {
+  forward.push(ctx.getImageData(0, 0, $canvas.width, $canvas.height));
 
-$(`.box[pid='7']`).click(function() {
-  color = 'white';
   
+    ctx.putImageData(back[back.length-1], 0, 0); 
+    back.pop()
 
-  ctx.beginPath()
+    ctx.beginPath()
+  
+});
+$(`.box[pid='12']`).click(function () {
+  back.push(ctx.getImageData(0, 0, $canvas.width, $canvas.height));
+
+  
+    ctx.putImageData(forward[forward.length-1], 0, 0); 
+    forward.pop()
+    ctx.beginPath()
+  
 });
 
+$(`.box[pid='7']`).click(function () {
+  color = 'white';
+  ctx.beginPath();
+});
 
 $(`.box[pid='5']`).click(function () {
   mouse = true;
@@ -308,32 +318,67 @@ $(`.box[pid='5']`).click(function () {
 });
 
 $("img[pid='1']").click(function () {
-
   if ($("img[pid='1']").attr("src") == "img/muted.png") {
-    $("img[pid='1']").attr("src", "img/sound.png")
-    $("img[pid='1']").attr("width", "+=50px")
+    $("img[pid='1']").attr("src", "img/sound.png");
+    $("img[pid='1']").attr("width", "+=50px");
     audio.play();
-
-  }
-
-  else {
-
-    $("img[pid='1']").attr("src", "img/muted.png")
-    $("img[pid='1']").attr("width", "-=50px")
+  } else {
+    $("img[pid='1']").attr("src", "img/muted.png");
+    $("img[pid='1']").attr("width", "-=50px");
     audio.pause();
   }
+});
 
+$('input').on('input', function () {
+  color = $('input')[0].value;
+  ctx.beginPath();
+});
+$('.box').on('mousedown touchstart', function() {
+  $(this).addClass('active');
+});
 
+$('.box').on('mouseup touchend', function() {
+  $(this).removeClass('active'); 
+});
+var cont=0;
 
-})
-
-
-$('input').on('input',function(){
-
+$(".box[pid='13']").click(function () {
  
-color=$('input')[0].value;
-ctx.beginPath();
+  ctx.beginPath()
 
-})
+if(cont<4){
+ 
+
+console.log(cont)
+if(cont==0){
+$(".box[pid='13'] img").attr('height','30px').attr('width','30px')
+thickness+=3
+}else 
+if(cont==1){
+  thickness+=3
+  $(".box[pid='13'] img").attr('height','50px').attr('width','50px')
+
+}
+if(cont==2){
+  thickness+=5
+  $(".box[pid='13'] img").attr('height','70px').attr('width','70px')
+
+}
+if(cont==3){
+  thickness+=5
+  $(".box[pid='13'] img").attr('height','80px').attr('width','80px')
+
+}
+
+cont+=1;
+}
+else{
+  cont=0;
+  thickness=5;
+  $(".box[pid='13'] img").attr('height','10px').attr('width','10px')
+
+}
 
 
+
+});
